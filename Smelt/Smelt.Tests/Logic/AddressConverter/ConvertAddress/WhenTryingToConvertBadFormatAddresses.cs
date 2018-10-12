e509eaae3921eba7d606c13f3413cec9c61d61e6
David Carroll <carroll.david.s@gmail.com>
2018-10-12 15:44:53 -0500

    Additional tests - Fix AddressConverter explanation text - Add tests for Hirom addresses - Add tests for Bad format addresses

---
+++ Smelt/Smelt.Tests/Logic/AddressConverter/ConvertAddress/WhenTryingToConvertBadFormatAddresses.cs
@@ -0,0 +1,25 @@
+﻿namespace Smelt.Tests.Logic.AddressConverter.ConvertAddress
+{
+    using System;
+    using Data;
+    using Shouldly;
+    using Smelt.Logic;
+    using Smelt.Logic.AddressConverter;
+
+    public class WhenTryingToConvertBadFormatAddresses
+    {
+        private readonly AppState appState;
+
+        public WhenTryingToConvertBadFormatAddresses()
+        {
+            appState = new AppState { Rom = new Rom() };
+            appState.Rom.Header.Makeup.Mapping = Mapping.LoRom;
+            appState.InputAddress = "12DEAD";
+        }
+
+        public void ShouldThrowException()
+        {
+            Should.Throw<Exception>(() => Command.Run(new ConvertAddressCommand(appState)));
+        }
+    }
+}
--- Smelt/Smelt/Forms/AddressConverter.Designer.cs
+++ Smelt/Smelt/Forms/AddressConverter.Designer.cs
@@ -39,18 +39,20 @@
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(296, 44);
+            this.label1.Size = new System.Drawing.Size(296, 59);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 61);
+            this.label2.Location = new System.Drawing.Point(16, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 1;
@@ -58,15 +60,17 @@
             // 
             // inputAddressTextBox
             // 
+            this.inputAddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.inputAddressTextBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.inputAddressTextBox.Location = new System.Drawing.Point(16, 77);
+            this.inputAddressTextBox.Location = new System.Drawing.Point(16, 88);
             this.inputAddressTextBox.Name = "inputAddressTextBox";
             this.inputAddressTextBox.Size = new System.Drawing.Size(100, 23);
             this.inputAddressTextBox.TabIndex = 2;
             // 
             // convertButton
             // 
-            this.convertButton.Location = new System.Drawing.Point(123, 77);
+            this.convertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.convertButton.Location = new System.Drawing.Point(123, 88);
             this.convertButton.Name = "convertButton";
             this.convertButton.Size = new System.Drawing.Size(75, 23);
             this.convertButton.TabIndex = 3;
@@ -76,8 +80,9 @@
             // 
             // outputAddressTextBox
             // 
+            this.outputAddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.outputAddressTextBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputAddressTextBox.Location = new System.Drawing.Point(204, 77);
+            this.outputAddressTextBox.Location = new System.Drawing.Point(204, 88);
             this.outputAddressTextBox.Name = "outputAddressTextBox";
             this.outputAddressTextBox.ReadOnly = true;
             this.outputAddressTextBox.Size = new System.Drawing.Size(100, 23);
@@ -85,8 +90,9 @@
             // 
             // label3
             // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(204, 61);
+            this.label3.Location = new System.Drawing.Point(204, 72);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 13);
             this.label3.TabIndex = 4;
@@ -96,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(321, 112);
+            this.ClientSize = new System.Drawing.Size(321, 123);
             this.Controls.Add(this.outputAddressTextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.convertButton);
